c02d6ae3fc61e49d4e901c8b1e3d28dd0882daa9
Catlinman <contact@catlinman.com>
2019-04-18 17:00:53 +0200

    Fix zsh-command-time plugin install directory

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -4,6 +4,6 @@
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone git://github.com/chrissicool/zsh-256color.git $ZSH_CUSTOM/plugins/zsh-256color
-git clone git://github.com/popstas/zsh-command-time.git $ZSH_CUSTOM/plugins/zsh-command-time
+git clone git://github.com/popstas/zsh-command-time.git $ZSH_CUSTOM/plugins/command-time
 git clone git://github.com/trapd00r/zsh-syntax-highlighting-filetypes.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting-filetypes
 mv $ZSH_CUSTOM/plugins/zsh-syntax-highlighting-filetypes/zsh-syntax-highlighting-filetypes{,.plugin}.zsh
